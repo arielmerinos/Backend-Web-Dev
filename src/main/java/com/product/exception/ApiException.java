703e3b4629fa54dd9b3468e3774f8315573a7a17
Restyled.io <commits@restyled.io>
2022-10-17 20:18:47 +0000

    Restyled by clang-format

--- src/main/java/com/product/exception/ApiException.java
+++ src/main/java/com/product/exception/ApiException.java
@@ -3,22 +3,17 @@ package com.product.exception;
 import org.springframework.http.HttpStatus;
 
 // En tiempo de ejecución (dentro de la JVM)
-public class ApiException extends RuntimeException{
+public class ApiException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
-    private HttpStatus status;
+  private static final long serialVersionUID = 1L;
+  private HttpStatus status;
 
+  public ApiException(HttpStatus status, String message) {
+    super(message);
+    this.status = status;
+  }
 
-    public ApiException(HttpStatus status, String message){
-        super(message);
-        this.status = status;
-    }
+  public HttpStatus getStatus() { return status; }
 
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
+  public void setStatus(HttpStatus status) { this.status = status; }
 }
